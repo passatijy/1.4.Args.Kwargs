07ccd10191db503e38f859ed3415f75eb61b3b76
Marat <lmar@mail.ru>
2019-08-28 19:27:22 +0300

    fixed

--- main.py
+++ main.py
@@ -55,11 +55,17 @@ class PhoneBook():
 		self.resultBook = itertools.filterfalse(lambda x: x.phone == searchphone, self.resultBook)
 		return self.resultBook
 	def fav_search(self):
-		k = 0
-		for elem in self:
-			k += 1
-	def name_search(self,*args):
-		pass
+		fav_list = []
+		for elem in self.resultBook:
+			if elem.is_favorite:
+				fav_list.append(elem)
+		return fav_list
+	def name_search(self,sname,lname):
+		search_list = []
+		for elem in self.resultBook:
+			if elem.shortname == sname and elem.longname == lname:
+				search_list.append(elem)
+		return search_list
 
 if __name__ == '__main__':
 	mycontact01 = Contact('Ivan','Petrov','1230101')
@@ -67,12 +73,27 @@ if __name__ == '__main__':
 	jhon = Contact('Jhon', 'Smith', '+71234567809', telegram='@jhony', email='jhony@smith.com')
 	print(jhon)
 	mycontact03 = Contact('Oleg','Smirnov','0000001', 'nothome','Kostroma', is_favorite = False ,email='olegsmirnov@kostroma.ru')
+	mycontact04 = Contact('Nik','Petrov','000002',is_favorite=True)
+	mycontact05 = Contact('Marshal','Konev','000003',is_favorite=True)
 	mycontactbook = PhoneBook('mybook')
 	mycontactbook.add(mycontact02)
 	mycontactbook.add(mycontact03)
+	mycontactbook.add(mycontact04)
+	mycontactbook.add(mycontact05)
 	print('My phonebook list: ', mycontactbook.show_all())
-	print('My phonebook type: ', type(mycontactbook))
 	print('My phonebook 0: ', next(mycontactbook))
+
+	print('*******Looking for favotites******')
+	for elem in mycontactbook.fav_search():
+		print('------FAV---CONTACT-----')
+		print(elem)
+	print('*******Looking shortname and longname "Oleg Smirnov"******')
+	for elem in mycontactbook.name_search('Jhon','Smith'):
+		print(elem)
+	for elem in mycontactbook.name_search('Sergey','Ivanov'):
+		print(elem)
+
+
 '''
 	for elem in mycontactbook.return_list():
 		print('Shortname:', elem.shortname, ' Longname:', elem.longname, ' phone:', elem.phone, ' Is favorite:', elem.is_favorite, 'Addition info:', elem.addition_info, ' Addition info2:', elem.addition_info_dict)
